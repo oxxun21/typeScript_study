bd5bbc8fedd92a4c23d4d4c222a27779cbfdef0e
Oh Ga Eun <dhrkdms4113@naver.com>
2022-11-10 20:52:45 +0900

    unknown_never_type

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// Type Alias(타입 알리아스): 타입 별칭을 사용하여 타입을 직접 생성할 수 있음
-// 유니온 타입을 반복하는 것은 번거롭기 때문에 유니온 타입을 저장할 수 있음
-// 유니온 타입의 동일한 타입이나 동일한 유형 설정을 참조시킴
-// 타입 설정이 필요한 코드 어디서나 사용할 수 있고, 타입의 별칭을 직접 설정함으로써 의도를 명확하게 반영할 수 있음
-// type Combinable = number | string;
-// type ConversionDescriptor = 'as-number' | 'as-text';
-// function combineLiteral(
-//     input1: Combinable, 
-//     input2:  Combinable,
-//     resultConversion: ConversionDescriptor
-// ) {}
-// 유니온 타입 외 객체 타입에도 별칭을 붙여 중복된 부분을 단순화시킬 수 있음 
-// before
-function greet(user) {
-    console.log('Hi, I am ' + user.name);
+////// unknown 타입
+var userInput; // 무엇을 입력할지 모름, 어떤 값이든 할당할 수 있지만 any 와는 다름
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput;    unknown타입은 string에 할당할 수 없다고 오류 발생, any 타입이라면 오류 발생 X
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function isOlder(user, checkAge) {
-    return checkAge > user.age;
+////// never 타입 (아직 정식으로 반영되진 않음, 명시적 설정은 가능)
+function generateError(message, code) {
+    // throw : 값, 객체를 넘김
+    throw { message: message, errorCode: code };
 }
-function greetAlias(user) {
-    console.log('Hi, I am ' + user.name);
-}
-function isOlderAlias(user, checkAge) {
-    return checkAge > user.age;
-}
+generateError('An error occurred', 500);
+// never를 명시함으로써 코드의 의도를 더 명확히 하고, 함수가 아무것도 반환하지 않음을 알리며 
+// 기본적으로 스크립트나 스크립트의 일부를 충돌시키거나 망가트리기 위한 것임을 알림
---
+++ app.ts
@@ -0,0 +1,22 @@
+////// unknown 타입
+let userInput: unknown;     // 무엇을 입력할지 모름, 어떤 값이든 할당할 수 있지만 any 와는 다름
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+// userName = userInput;    unknown타입은 string에 할당할 수 없다고 오류 발생, any 타입이라면 오류 발생 X
+
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+
+////// never 타입 (아직 정식으로 반영되진 않음, 명시적 설정은 가능)
+function generateError(message: string, code: number): never {
+    // throw : 값, 객체를 넘김
+    throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred', 500);
+// never를 명시함으로써 코드의 의도를 더 명확히 하고, 함수가 아무것도 반환하지 않음을 알리며 
+// 기본적으로 스크립트나 스크립트의 일부를 충돌시키거나 망가트리기 위한 것임을 알림
