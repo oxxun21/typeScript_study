6bb6b519c9ea033786738e7510f025c844a1ba5d
Oh Ga Eun <dhrkdms4113@naver.com>
2022-11-27 19:23:21 +0900

    modify

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,1 @@
 "use strict";
-// optional chaining (옵셔널 체이닝) :
-const fetchedUserDate = {
-    id: 'u1',
-    name: 'Max',
-    job: { title: 'CEO', description: 'My own company' }
-};
-console.log(fetchedUserDate.job && fetchedUserDate.job.title);
