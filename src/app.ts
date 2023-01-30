1a81051663a9d5697f8bb542a418e211b1898e2b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-01-30 18:19:37 +0900

    axios install

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  axios.get();
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+      ).then(response => {console.log(response)}
+      ).catch(err => {console.log(err)});
 }
 
 form.addEventListener('submit', searchAddressHandler);
